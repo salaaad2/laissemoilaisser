d1232b7b014859e0f3977895347c6392e2a09483
salaaad2 <arthurdurant263@gmail.com>
2021-11-07 17:38:05 +0100

    add folder info

--- src/e_open.c
+++ src/e_open.c
@@ -27,12 +27,17 @@ int e_open(t_elem * elem)
         while (de != NULL)
         {
             exists = stat(de->d_name, &buf);
-            if (exists < 0)
-            {
+            if (exists < 0) {
                 ft_dprintf(2, "ls: %s: File not found\n", de->d_name);
-            }
-            else
-            {
+            } else if (S_ISDIR(buf.st_mode)) {
+                ft_printf("%10ld %s/\n", buf.st_size, de->d_name);
+            } else if (S_ISDIR(buf.st_mode)) {
+                ft_printf("%10ld %s/\n", buf.st_size, de->d_name);
+            } else if (S_ISLNK(buf.st_mode)) {
+                ft_printf("%10ld %s@\n", buf.st_size, de->d_name);
+            } else if (buf.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) {
+                ft_printf("%10ld %s@\n", buf.st_size, de->d_name);
+            } else {
                 ft_printf("%10ld %s\n", buf.st_size, de->d_name);
             }
             de = readdir(d);
