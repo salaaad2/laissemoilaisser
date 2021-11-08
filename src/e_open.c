0d75d069b74887cd1304896d1e6f1cf2cc64128c
salaaad2 <salaaad2@gitlab.com>
2021-11-08 16:43:35 +0100

    yessaye my friend

--- src/e_open.c
+++ src/e_open.c
@@ -29,6 +29,8 @@ e_open(t_elem * elem, t_opts * opts)
     while (node != NULL)
     {
         ft_bzero(node->outbuf, sizeof(node->outbuf));
+        ft_bzero(node->path, sizeof(node->path));
+        ft_bzero(node->name, sizeof(node->name));
         d = opendir((char*)node->content);
         if (d == NULL) {
             ft_dprintf(2, "ls: %s: unable to open%s\n", (char*)node->content, strerror(errno));
@@ -37,30 +39,30 @@ e_open(t_elem * elem, t_opts * opts)
 
         while ((de = readdir(d)) != NULL)
         {
-          if (ft_strcmp(elem->content, ".") != 0) {
-              ft_sprintf(elem->path, "%s/", (char *)elem->content);
-              ft_sprintf(elem->name, "%s", (char *)de->d_name);
-              ft_sprintf(full, "%s/%s", elem->path, elem->name);
+          if (ft_strcmp(node->content, ".") != 0) {
+              ft_sprintf(node->path, "%s", (char *)node->content);
+              ft_sprintf(node->name, "%s", (char *)de->d_name);
+              ft_sprintf(full, "%s/%s", node->path, node->name);
           } else {
-              ft_sprintf(elem->name, "%s", (char *)de->d_name);
-              ft_sprintf(full, "%s", elem->name);
+              ft_sprintf(node->name, "%s", (char *)de->d_name);
+              ft_sprintf(full, "%s", node->name);
           }
           exists = stat(full, &buf);
           if (exists  < 0) {
-            ft_dprintf(2, "ls: %s: File not found %s\n", de->d_name,
+            ft_dprintf(2, "ls: %s: File not found %s\n", full,
                        strerror(errno));
-          } else if (elem->name[0] == '.' &&
+          } else if (node->name[0] == '.' &&
                      opts->hidden == FALSE) { /* skip entry if file is hidden */
             continue;
           } else if (S_ISDIR(buf.st_mode)) {
             if (opts->recursive == FALSE)
-              ft_sprintf(node->outbuf, "%s %s ", node->outbuf, elem->name);
+              ft_sprintf(node->outbuf, "%s %s ", node->outbuf, node->name);
           } else if (S_ISLNK(buf.st_mode)) {
-            ft_sprintf(node->outbuf, "%s %s ", node->outbuf, elem->name);
+            ft_sprintf(node->outbuf, "%s %s ", node->outbuf, node->name);
           } else if (buf.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) {
-            ft_sprintf(node->outbuf, "%s %s ", node->outbuf, elem->name);
+            ft_sprintf(node->outbuf, "%s %s ", node->outbuf, node->name);
           } else {
-            ft_sprintf(node->outbuf, "%s %s ", node->outbuf, elem->name);
+            ft_sprintf(node->outbuf, "%s %s ", node->outbuf, node->name);
           }
         }
         ft_dprintf(1, "%s\n", node->outbuf);
